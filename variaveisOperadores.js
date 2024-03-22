55f3a18dd5132c3e75381125fc4b2137f796936d
Nicolas Lopes <108310195+Nicolaswav@users.noreply.github.com>
2024-03-22 09:40:14 -0300

    Add files via upload

---
+++ variaveisOperadores.js
@@ -0,0 +1,22 @@
+// Declaração das variáveis numéricas e cálculo dos resultados
+let numA1 = 10;
+let numB1 = 5;
+let soma = numA1 + numB1;
+
+let numA2 = 8;
+let numB2 = 3;
+let subtracao = numA2 - numB2;
+
+let numA3 = 6;
+let numB3 = 2;
+let multiplicacao = numA3 * numB3;
+
+let numA4 = 20;
+let numB4 = 4;
+let divisao = numA4 / numB4;
+
+// Impressão dos resultados
+console.log(Resultado da soma entre ${numA1} e ${numB1}: ${soma});
+console.log(Resultado da subtração entre ${numA2} e ${numB2}: ${subtracao});
+console.log(Resultado da multiplicação entre ${numA3} e ${numB3}: ${multiplicacao});
+console.log(Resultado da divisão entre ${numA4} e ${numB4}: ${divisao});
